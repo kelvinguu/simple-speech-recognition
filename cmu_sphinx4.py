0b5e29328cb37d70e1b7de9f6e90f38ef2d030b8
Kelvin Gu <gu.kelvin@gmail.com>
2013-12-11 15:46:22 -0800

    added default parameters to Transcriber. Working.

--- cmu_sphinx4.py
+++ cmu_sphinx4.py
@@ -3,13 +3,38 @@ import pexpect
 import os
 import inspect
 
-class Transcriber:
-    def __init__(self, audio_URL, parameters):
+def from_module_dir(relative_path):
+    # TODO: may not work for every OS
+    module_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+    abs_path =  os.path.join(module_dir, relative_path.lstrip('\/'))
+    return abs_path
+
+class Transcriber(object):
+    lang_model = from_module_dir('models/language_model.arpaformat.DMP')
+    accoustic_model = from_module_dir('models/hub4opensrc.cd_continuous_8gau')
+    dictionary = from_module_dir('models/cmudict.0.7a_SPHINX_40')
+    filler = from_module_dir('models/wsj_noisedict')
+
+    default_params = {
+        'absoluteBeamWidth': '700',
+        'absoluteWordBeamWidth': '100',
+        'relativeBeamWidth': '1E-80',
+        'relativeWordBeamWidth': '1E-60',
+        'wordInsertionProbability': '0.2',
+        'silenceInsertionProbability': '.1',
+        'languageWeight': '10.5',
+        'languageModelLocation': lang_model,
+        'acousticModelLocation': accoustic_model,
+        'dictionaryPath': dictionary,
+        'fillerPath': filler
+        }
+
+    def __init__(self, audio_URL, parameters=default_params):
         self.audio_URL = audio_URL
-        self.config_path = self.from_module_dir('config_{}.xml'.format(id(self)))
+        self.config_path = from_module_dir('config_{}.xml'.format(id(self)))
 
         # load text from config_template.xml
-        template_path = self.from_module_dir('config_template.xml')
+        template_path = from_module_dir('config_template.xml')
         with open(template_path, 'r') as input_file:
             text = input_file.read()
 
@@ -28,16 +53,10 @@ class Transcriber:
         except OSError:
             pass
 
-    def from_module_dir(self, relative_path):
-        # TODO: may not work for every OS
-        module_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        abs_path =  os.path.join(module_dir, relative_path.lstrip('\/'))
-        return abs_path
-
     def transcript_stream(self):
         # construct class_path
-        sphinx_jar_path = self.from_module_dir('sphinx4.jar')
-        class_path = self.from_module_dir('') + ":" + sphinx_jar_path
+        sphinx_jar_path = from_module_dir('sphinx4.jar')
+        class_path = from_module_dir('') + ":" + sphinx_jar_path
         config_URL = 'file://localhost' + self.config_path
 
         # execute CMU Sphinx
